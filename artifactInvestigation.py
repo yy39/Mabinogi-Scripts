9de41dcd611fca7999f158a8808c86358ce91fb8
yy39 <yusanyin@gmail.com>
2024-02-20 15:56:52 -0800

    add artifactInvestigation

---
+++ artifactInvestigation.py
@@ -0,0 +1,3 @@
+# Cooldown for Artifact Investigation is 180s from rank N to B, 120s from rank A to 8, 60s from 7 to 5, and 30s from 3 to 1
+# could also use open-cv to determine when Artifact Investigation is off cooldown, but seems a bit more intensive
+

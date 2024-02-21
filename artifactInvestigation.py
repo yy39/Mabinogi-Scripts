e312d5590cdf5b9d86cfbc2d95da56751393bdee
yy39 <yusanyin@gmail.com>
2024-02-21 15:19:12 -0800

    add initial functions for artifactInvestigation

--- artifactInvestigation.py
+++ artifactInvestigation.py
@@ -9,8 +9,12 @@
 # left click ok
 # wait for number of sec depending on skill rank
 # this process is the same for both artifact appraisal and restoration, with slightly different window prompts
+import time
+import pyautogui
+import config
+import autoMouse
 
-def determineCD(rank):
+def determineSleep(rank):
     match rank:
         case '1' | '2' | '3' :
             return 30
@@ -20,3 +24,27 @@ def determineCD(rank):
             return 120
         case default:
             return 180
+
+#may need to download all possible artifact pngs
+def clickOnArtifacts(image):
+    try: 
+        imageCoords = pyautogui.locateCenterOnScreen(image, confidence=config.imageConfidence)
+    except Exception as e:
+        print(f'{type(e).__name__}: Unable to find {image}')
+        if retryCount == config.retryMax:
+            print('Max retries hit')
+            retryCount = 0
+            exit()
+        retryCount = retryCount + 1
+        print(f'Sleeping for {config.sleepDuration} seconds... Then retrying. Attempt {retryCount} of {config.retryMax}')
+        time.sleep(config.sleepDuration)
+        autoMouse.clickImage(image, retryCount)
+    else:
+        print(f'image found: {image}')
+        retryCount = 0
+        autoMouse.clickAt(imageCoords[0], imageCoords[1])
+
+# Select Mabinogi window
+autoMouse.clickMainCenter()
+autoMouse.clickImage('images/artifactInvestigationIcon.png')
+#sleep = determineSleep()

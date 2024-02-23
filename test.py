a4309ee252c051ee0921d83e9bf94330d8f6c854
yy39 <yusanyin@gmail.com>
2024-02-22 22:07:08 -0800

    trying grayscale img

--- test.py
+++ test.py
@@ -4,7 +4,9 @@ import pytesseract
 pytesseract.pytesseract.tesseract_cmd = 'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 img = cv2.imread('test.png')
+grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 cv2.imshow('test.png', img)
+cv2.imshow('testGrayscale.png', grayImg)
 cv2.waitKey(0)
 cv2.destroyAllWindows()

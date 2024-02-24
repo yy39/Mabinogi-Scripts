97073e6ba9fde7fc9ce3c764daed0746c8ac09f8
yy39 <yusanyin@gmail.com>
2024-02-23 21:21:44 -0800

    playing with thresholds

--- test.py
+++ test.py
@@ -6,7 +6,10 @@ pytesseract.pytesseract.tesseract_cmd = 'C:\Program Files\Tesseract-OCR\tesserac
 img = cv2.imread('test.png')
 grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+ret, thresh = cv2.threshold(img, 220, 255, cv2.THRESH_BINARY) 
+
 cv2.imshow('test.png', img)
 cv2.imshow('testGrayscale.png', grayImg)
+cv2.imshow('threshold', thresh)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
